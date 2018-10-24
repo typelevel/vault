a4bc39a82fdee6ffb14ba32a13a3867c9fbbce30
Chris Davenport <chris@christopherdavenport.tech>
2018-10-24 13:20:41 -0400

    Fix Scaladoc

--- src/main/scala/io/chrisdavenport/vault/Key.scala
+++ src/main/scala/io/chrisdavenport/vault/Key.scala
@@ -6,5 +6,8 @@ import cats.implicits._
 final case class Key[A] private[vault] (private[vault] unique: Unique)
 
 object Key {
+  /**
+   * Create A Typed Key
+   */
   def newKey[F[_]: Sync, A]: F[Key[A]] = Unique.newUnique[F].map(Key[A])
 }
--- src/main/scala/io/chrisdavenport/vault/Locker.scala
+++ src/main/scala/io/chrisdavenport/vault/Locker.scala
@@ -1,22 +1,25 @@
 package io.chrisdavenport.vault
 
 /**
-  * Locker - A persistent store for a single value
-  **/
+ * Locker - A persistent store for a single value
+ **/
 final case class Locker private[vault] (private[vault] unique: Unique, private[vault] a: Any){
+  /**
+   * Retrieve the value from the Locker
+   */
   def unlock[A](k: Key[A]): Option[A] = Locker.unlock(k, this)
 }
 object Locker {
   /**
-    * Put a single value into a Locker
-    **/
+   * Put a single value into a Locker
+   */
   def lock[A](k: Key[A], a: A): Locker = k match {
     case Key(u) => Locker(u, a.asInstanceOf[Any])
   }
 
   /**
-    * Retrieve the value from the Locker
-    **/
+   * Retrieve the value from the Locker
+   */
   def unlock[A](k: Key[A], l: Locker): Option[A] = (k, l) match {
     // Equality By Reference Equality
     case (Key(u1), Locker(u2, a)) if u1 == u2 => 
