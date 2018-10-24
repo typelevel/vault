791c207bd13415d0d0bfcf2499f0068153cb98b1
Chris Davenport <chris@christopherdavenport.tech>
2018-10-24 10:53:28 -0400

    Add README and Move to Companions

--- src/main/scala/io/chrisdavenport/vault/Vault.scala
+++ src/main/scala/io/chrisdavenport/vault/Vault.scala
@@ -14,16 +14,25 @@ object Vault {
     * Lockers
     **/
   final case class Key[A] private (private[vault] unique: Unique, private[vault] a: Ref[IO, Option[A]])
-  final case class Locker private (private[vault] unique: Unique, private[vault] a: IO[Unit])
-
-  def lock[A](k: Key[A], a: A): Locker = k match {
-    case Key(u, ref) => Locker(u, ref.update(_ => Some(a)))
+  object Key {
+    def newKey[A]: IO[Key[A]] = for {
+      unique <- Unique.newUnique[IO]
+      ref <- Ref.of[IO, Option[A]](None)
+    } yield Key[A](unique, ref)
   }
-  def unlock[A](k: Key[A], l: Locker): Option[A] = (k, l) match {
-    // Equality By Reference Equality
-    case (Key(u1, ref), Locker(u2, m)) if u1 == u2 => 
-      (m >> ref.get).unsafeRunSync // Race Condition
-    case _ => None
+  final case class Locker private (private[vault] unique: Unique, private[vault] a: IO[Unit]){
+    def unlock[A](k: Key[A]): Option[A] = Locker.unlock(k, this)
+  }
+  object Locker {
+    def lock[A](k: Key[A], a: A): Locker = k match {
+      case Key(u, ref) => Locker(u, ref.update(_ => Some(a)))
+    }
+    def unlock[A](k: Key[A], l: Locker): Option[A] = (k, l) match {
+      // Equality By Reference Equality
+      case (Key(u1, ref), Locker(u2, m)) if u1 == u2 => 
+        (m >> ref.get).unsafeRunSync // Race Condition
+      case _ => None
+    }
   }
 
   /**
@@ -31,24 +40,18 @@ object Vault {
     * Implemented as a collection of lockers
     **/
   final case class Vault private (private[vault] m: Map[Unique, Locker])
+  object Vault {
+    def empty = Vault(Map.empty)
+    def lookup[A](k: Key[A], v: Vault): Option[A] = (k, v) match {
+      case (key@Key(k, _), Vault(m)) => m.get(k).flatMap{locker => Locker.unlock(key, locker)}
+    }
 
-  def empty = Vault(Map.empty)
-
-  def newKey[A]: IO[Key[A]] = for {
-    unique <- Unique.newUnique[IO]
-    ref <- Ref.of[IO, Option[A]](None)
-  } yield Key[A](unique, ref)
-
-  def lookup[A](k: Key[A], v: Vault): Option[A] = (k, v) match {
-    case (key@Key(k, _), Vault(m)) => m.get(k).flatMap{locker => unlock(key, locker)}
-  }
-
-  def insert[A](k: Key[A], a:A, v: Vault): Vault = (k, v) match {
-    case (key@Key(k, _), Vault(m)) => Vault(m + (k -> lock(key, a)))
-  }
-
-  def delete[A](k: Key[A], v: Vault): Vault = (k, v) match {
-    case (Key(k, _), Vault(m)) => Vault(m - k)
+    def insert[A](k: Key[A], a:A, v: Vault): Vault = (k, v) match {
+      case (key@Key(k, _), Vault(m)) => Vault(m + (k -> Locker.lock(key, a)))
+    }
+    def delete[A](k: Key[A], v: Vault): Vault = (k, v) match {
+      case (Key(k, _), Vault(m)) => Vault(m - k)
+    }
   }
 
 }
