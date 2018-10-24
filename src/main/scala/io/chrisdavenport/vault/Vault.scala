3850ca4fec0a5608c6d9f5a847a236e362b5ac39
Chris Davenport <chris@christopherdavenport.tech>
2018-10-24 09:50:58 -0400

    Lock Stuff Down

--- src/main/scala/io/chrisdavenport/vault/Vault.scala
+++ src/main/scala/io/chrisdavenport/vault/Vault.scala
@@ -6,13 +6,12 @@ import cats.effect.IO
 import cats.effect.concurrent.Ref
 
 object Vault {
-  final case class Unique(i: Int)
-  object Unique {
+  private[vault] final case class Unique (i: Int)
+  private[vault] object Unique {
     implicit val uniqueInstances: Eq[Unique] = Eq.by(_.i)
 
     // Global Source of Uniqueness
     private lazy val uniqueSource: Ref[IO, Int] = Ref.of[IO, Int](0).unsafeRunSync
-
     def newUnique: IO[Unique] = uniqueSource.modify{value => 
       val z = value + 1 
       (z,z)
@@ -23,8 +22,8 @@ object Vault {
   /**
     * Lockers
     **/
-  final case class Key[A](private[vault] unique: Unique, private[vault] a: Ref[IO, Option[A]])
-  final case class Locker(private[vault] unique: Unique, private[vault] a: IO[Unit])
+  final case class Key[A] private (private[vault] unique: Unique, private[vault] a: Ref[IO, Option[A]])
+  final case class Locker private (private[vault] unique: Unique, private[vault] a: IO[Unit])
 
   def lock[A](k: Key[A], a: A): Locker = k match {
     case Key(u, ref) => Locker(u, ref.update(_ => Some(a)))
@@ -39,7 +38,7 @@ object Vault {
     * Vault
     * Implemented as a collection of lockers
     **/
-  final case class Vault(private[vault] m: Map[Unique, Locker])
+  final case class Vault private (private[vault] m: Map[Unique, Locker])
 
   def empty = Vault(Map.empty)
 
