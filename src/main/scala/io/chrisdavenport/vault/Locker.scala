356c2985a94906b762fd2c63510e8b1889a805ab
Chris Davenport <chris@christopherdavenport.tech>
2018-11-01 13:49:27 -0400

    Remove Case Classes for better Interactions

--- src/main/scala/io/chrisdavenport/vault/Locker.scala
+++ src/main/scala/io/chrisdavenport/vault/Locker.scala
@@ -3,27 +3,24 @@ package io.chrisdavenport.vault
 /**
  * Locker - A persistent store for a single value
  **/
-final case class Locker private[vault] (private[vault] unique: Unique, private[vault] a: Any){
+final class Locker private[vault] (private[vault] val unique: Unique, private[vault] val a: Any){
   /**
    * Retrieve the value from the Locker
    */
   def unlock[A](k: Key[A]): Option[A] = Locker.unlock(k, this)
 }
+
 object Locker {
   /**
    * Put a single value into a Locker
    */
-  def lock[A](k: Key[A], a: A): Locker = k match {
-    case Key(u) => Locker(u, a.asInstanceOf[Any])
-  }
+  def lock[A](k: Key[A], a: A): Locker = new Locker(k.unique, a.asInstanceOf[Any])
 
   /**
    * Retrieve the value from the Locker
    */
-  def unlock[A](k: Key[A], l: Locker): Option[A] = (k, l) match {
+  def unlock[A](k: Key[A], l: Locker): Option[A] = 
     // Equality By Reference Equality
-    case (Key(u1), Locker(u2, a)) if u1 == u2 => 
-      Some(a.asInstanceOf[A])
-    case _ => None
-  }
+    if (k.unique == l.unique) Some(l.a.asInstanceOf[A])
+    else None
 }
