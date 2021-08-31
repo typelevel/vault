34c6ddbf76ecd728fea5531826732bf044b39fce
Rob Norris <rob_norris@mac.com>
2021-08-31 14:35:55 -0500

    bincompat

--- core/src/main/scala/org/typelevel/vault/Key.scala
+++ core/src/main/scala/org/typelevel/vault/Key.scala
@@ -31,24 +31,26 @@ import cats.Invariant
  * A unique value tagged with a specific type to that unique. Since it can only be created as a result of that, it links
  * a Unique identifier to a type known by the compiler.
  */
-sealed trait Key[A] { outer =>
-  private[vault] def unique: Unique.Token
-  private[vault] type I
-  private[vault] def in: A => I
-  private[vault] def out: I => A
-  override def hashCode(): Int = unique.hashCode()
+final class Key[A] private (
+  private[vault] val unique: Unique.Token,
+  private[vault] val imapping: InvariantMapping[A]
+) {
+
+  private[vault] type I = imapping.I
+  private[vault] val in = imapping.in
+  private[vault] val out = imapping.out
+
+  // Overloaded ctor to preserve bincompat
+  def this(unique: Unique.Token) =
+    this(unique, InvariantMapping.id[A])
 
   /**
    * Create a copy of this key that references the same underlying vault element, transformed from
    * type `B` before insert, and to `B` after lookup.
    */
-  def imap[B](f: A => B)(g: B => A): Key[B] =
-    new Key[B] {
-      val unique = outer.unique
-      type I = outer.I
-      val in = g andThen outer.in
-      val out = outer.out andThen f
-    }
+  def imap[B](f: A => B)(g: B => A): Key[B] = new Key(unique, imapping.imap(f)(g))
+
+  override def hashCode(): Int = unique.hashCode()
 
 }
 
@@ -57,14 +59,7 @@ object Key {
   /**
    * Create A Typed Key
    */
-  def newKey[F[_]: Functor: Unique, A]: F[Key[A]] = Unique[F].unique.map { u =>
-    new Key[A] {
-      val unique = u
-      type I = A
-      val in = identity
-      val out = identity
-    }
-  }
+  def newKey[F[_]: Functor: Unique, A]: F[Key[A]] = Unique[F].unique.map(new Key[A](_))
 
   implicit def keyInstances[A]: Hash[Key[A]] = new Hash[Key[A]] {
     // Members declared in cats.kernel.Eq
